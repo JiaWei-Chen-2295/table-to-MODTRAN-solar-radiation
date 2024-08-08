6fc8f55b4c9cbba6e13c4a5ad8e144e7f2859444
JiaWei-Chen-2295 <1601020332@qq.com>
2024-08-08 22:12:02 +0800

    Create .venv batch and create requirements

--- setup.bat
+++ setup.bat
@@ -1,3 +1,18 @@
 @echo off
+
+
+:: 创建虚拟环境
+python -m venv .venv
+
+:: 激活虚拟环境
+call .venv\Scripts\activate
+
+:: 安装 requirements.txt 中的依赖
+pip install -r requirements.txt
+
+:: 输出确认消息
+echo Virtual environment created and dependencies installed.
+
 .venv\Scripts\python.exe setup.py
+
 pause
